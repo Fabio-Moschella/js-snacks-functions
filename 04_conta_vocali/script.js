c5b132cc6274828dc0d627403b767f323cd7fc23
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-08 18:26:19 +0200

    add-four-track

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,27 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+function VowelsOfTheWord(string) {
+  let vowelsNumber = 0;
+  for (let i = 0; i < string.length; i++) {
+    const vowels = string[i].toLowerCase();
+    if (
+      vowels === "a" ||
+      vowels === "e" ||
+      vowels === "i" ||
+      vowels === "o" ||
+      vowels === "u"
+    ) {
+      vowelsNumber += 1;
+    }
+  }
+  return vowelsNumber;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(VowelsOfTheWord(word) + "(a, a, i)");
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
