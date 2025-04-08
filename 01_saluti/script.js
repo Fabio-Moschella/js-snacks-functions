2cb6efd83f46616f00900d9c8c4aa764f8ca78bb
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-08 18:48:25 +0200

    add-first-track

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,7 +4,8 @@ const userName = "Mario";
 
 // Dichiara la funzione qui.
 function helloUser(name) {
-  return "ciao" + " " + name;
+  let saluto = "ciao";
+  return saluto + " " + name;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
