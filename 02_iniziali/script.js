491e8659df3fac627146cf4e105022d09e0c9995
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-08 16:06:30 +0200

    first commit

---
+++ 02_iniziali/script.js
@@ -0,0 +1,15 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+// Dichiara la funzione qui.
+function initialNamesOfTheArray(namesArray) {
+ newArray = ["A", "L", "M", "A", "G", "A"];
+    if(namesArray === newArray)
+        return fal
+  
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+const initialNamesOfTheArray(names);
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
