8d86db1838f24b24998cbf852464bf0451729226
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-08 17:35:31 +0200

    add-third-track

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const char = "A";
 
 // Dichiara la funzione qui.
+function namesList(name, letter) {
+  const newArray = [];
+  let ucLetter = letter.toUpperCase();
+  for (let i = 0; i < name.length; i++) {
+    firstCharName = name[i];
 
+    if (firstCharName[0] === ucLetter) {
+      newArray.push(firstCharName);
+    }
+  }
+  return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(namesList(names, char));
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
